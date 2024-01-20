8d592ef79152e674731781c0d9817cccfd61f886
Alexsander <alexnogueira4@gmail.com>
2024-01-20 13:29:28 -0300

    adjust imports

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import Database from './config/database'
 import server from './server/server'
 // import rooms from './api/rooms'
 // import eletronics from './api/eletronics'
-import entertainment from './api/entertainment'
+import {Entertainment, Commando} from './api'
 import express from "express";
 import morgan from 'morgan'
 import helmet from 'helmet'
@@ -20,7 +20,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // server.start(app, rooms, database, () => { console.log('Started rooms') })
 // server.start(app, eletronics, database, () => { console.log('Started eletronic') })
-server.start(app, entertainment, database, () => { console.log('Started eletronic') })
+server.start(app, Entertainment, database, () => { console.log('Started eletronic') })
+server.start(app, Commando, database, () => { console.log('Started eletronic') })
 
 app.listen(process.env.PORT, () => {
   console.log('teste funcional, porta: ', process.env.PORT);
